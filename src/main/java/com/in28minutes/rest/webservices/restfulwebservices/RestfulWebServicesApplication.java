09838236fe3375ef0c8817fe95b30843bb9df98f
amunozpe@vectoritcgroup.com <amunozpe@vectoritcgroup.com>
2020-05-13 12:05:32 +0200

    up

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
@@ -2,9 +2,10 @@ package com.in28minutes.rest.webservices.restfulwebservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 public class RestfulWebServicesApplication {
 
 	public static void main(String[] args) {
